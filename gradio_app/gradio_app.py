28ecd7be398b408fd8d83daa3d5160d2976604e3
ellenmartin11 <martin.ellenjane@gmail.com>
2025-04-30 17:05:31 -0400

    improved gradio app and added line plot, also restricted scatter plot vars to continuous, and line plot to categorical

--- gradio_app/gradio_app.py
+++ gradio_app/gradio_app.py
@@ -6,12 +6,8 @@ import seaborn as sns
 # Load your clean dataset
 obesity_data_clean = pd.read_csv("obesity_data_clean.csv")
 
+scatter_vars = ['age', 'height', 'weight']
 def plot_correlation(var1, var2):
-
-    forbidden_vars = ['gender','obesity_group','transport']
-
-    if var1 in forbidden_vars or var2 in forbidden_vars:
-        return None, f"Error: You selected a categorical variable ({var1} or {var2}). Please select continuous variables."
     if var1 is None or var2 is None:
         return None
     plt.figure(figsize=(6, 6))
@@ -23,7 +19,7 @@ def plot_correlation(var1, var2):
     path = "scatter.png"
     plt.savefig(path)
     plt.close()
-    return path, ""
+    return path
 
 def plot_heatmap(cols):
     forbidden_vars = ['gender','obesity_group','transport']
@@ -46,17 +42,56 @@ def plot_heatmap(cols):
     plt.close()
     return path, ""
 
+numerical_variables = ['time_technology','age','height','weight','freq_veggie','amt_daily_meals','freq_snacking','amt_water','exercise_per_week','freq_alcohol']
+
+
+def plot_lineplot(y_var):
+    if y_var is None:
+        return None
+    grouped_data = obesity_data_clean.groupby(['obesity_group'])
+    var_mean = grouped_data[y_var].mean()
+    var_sem = grouped_data[y_var].sem()
+
+    order = ['insufficient_weight', 
+         'normal_weight',
+         'overweight_level_i', 
+         'overweight_level_ii',
+         'obesity_type_i', 
+         'obesity_type_ii', 
+         'obesity_type_iii']
+    
+    obesity_var_mean = var_mean.reindex(order)
+    obesity_var_sem = var_sem.reindex(order)
+    plt.figure(figsize=(8,6))
+    plt.errorbar(x=obesity_var_mean.index, y=obesity_var_mean, yerr=obesity_var_sem, fmt='-o', capsize=5)
+    plt.xlabel('Obesity Category')
+    plt.ylabel(f'Mean {y_var}')
+    plt.title(f'Obesity and {y_var}')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    path = "lineplot.png"
+    plt.savefig(path)
+    plt.close()
+    return path
+
+
 with gr.Blocks(theme = gr.themes.Soft()) as demo:
     gr.Markdown("# 🧪 Obesity Dataset Explorer")
 
     with gr.Tab("Scatterplot"):
         gr.Markdown("Explore correlation between two variables. Pick from the drop-downs below.")
-        var1 = gr.Dropdown(label="X-axis Variable", choices=obesity_data_clean.columns.tolist())
-        var2 = gr.Dropdown(label="Y-axis Variable", choices=obesity_data_clean.columns.tolist())
+        var1 = gr.Dropdown(label="X-axis Variable", choices=scatter_vars)
+        var2 = gr.Dropdown(label="Y-axis Variable", choices=scatter_vars)
         scatter_btn = gr.Button("Generate Scatterplot")
         scatter_output = gr.Image()
-        scatter_error = gr.Textbox(label="Error Message", interactive=False)
-        scatter_btn.click(fn=plot_correlation, inputs=[var1, var2], outputs=[scatter_output,scatter_error])
+        scatter_btn.click(fn=plot_correlation, inputs=[var1, var2], outputs=scatter_output)
+
+    with gr.Tab("Lineplot"):
+        gr.Markdown("Explore how different features vary across the obesity groups.")
+        y_var = gr.Dropdown(label="Y-axis Variable", choices = numerical_variables)
+        line_btn = gr.Button("Generate Lineplot")
+        line_output = gr.Image()
+        line_btn.click(fn=plot_lineplot, inputs=[y_var], outputs=line_output)
         
 
     with gr.Tab("Heatmap"):
